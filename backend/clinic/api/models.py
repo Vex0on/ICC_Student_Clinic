5b6791f2745c2e3d2f1c84a6cb24de08490fd90d
MadPapa <happyekkov@gmail.com>
2023-05-24 13:10:15 +0200

    create models User, Student, first migration

--- backend/clinic/api/models.py
+++ backend/clinic/api/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.CharField(max_length=45, null=False, unique=True)
+    password = models.CharField(max_length=45, null=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.email
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=45, null=False)
+    last_name = models.CharField(max_length=45, null=False)
+    date_of_birth = models.DateField()
+    pesel = models.CharField(max_length=11, null=False, unique=True)
+    phone_number = models.CharField(max_length=9, null=False, unique=True)
+    address = models.CharField(max_length=95, null=False)
+    index_number = models.CharField(max_length=6, null=False, unique=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.index_number}"
